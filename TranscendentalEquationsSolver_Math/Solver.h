8f77fce9b621861830cde2f34524a3fcba3bf1c5
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-02-12 13:17:04 +0200

    Fix unused sumOfError variable

--- TranscendentalEquationsSolver_Math/Solver.h
+++ TranscendentalEquationsSolver_Math/Solver.h
@@ -52,53 +52,7 @@ public:
          index += (one / pow(10, currentPrecision));
       }
 
-      return index;//(abs(number) < abs(lastNumber)) ? abs(number) : abs(lastNumber);
-   }
-
-   double solveDep()
-   {
-      long double index = 0;
-      int currentPrecision = 0;
-      bool lastSign;
-      long double lastNumber;
-      long double number;
-
-      lastNumber = separateRootDep(index);
-      lastSign = (lastNumber > 0) ? true : false;
-      index = index + one / pow(10, currentPrecision);
-
-      while (currentPrecision != precision)
-      {
-         number = separateRootDep(index);
-         bool sign = (number > 0) ? true : false;
-
-         if (sign != lastSign)
-         {
-            index = (abs(number) < abs(lastNumber)) ? index : index + (one / pow(10, currentPrecision)) * (-1);
-
-            long double before = index;
-
-            currentPrecision++;
-
-            long double tail = (one / pow(10, currentPrecision));
-
-            index += tail;
-
-            checkDivisionError(before, index, currentPrecision);
-
-            lastNumber = separateRootDep(index);
-            lastSign = (lastNumber > 0) ? true : false;
-         }
-         else
-         {
-            lastNumber = number;
-            lastSign = sign;
-         }
-
-         index += (one / pow(10, currentPrecision));
-      }
-
-      return index;//(abs(number) < abs(lastNumber)) ? abs(number) : abs(lastNumber);
+      return index;
    }
 
 private:
@@ -107,17 +61,12 @@ private:
    long double secondNumber;
    long double thirdNumber;
 
-   int precision = 5;
+   int precision = 4;
    long double one = 1;
 
    double separateRoot(double number)
    {
       return add(new long double[3]{ pow(number * firstNumber, fnPower), (secondNumber * number), thirdNumber }, 3);
-   }
-
-   double separateRootDep(double number)
-   {
-      return pow(number * firstNumber, fnPower) + (secondNumber * number) + thirdNumber;
    }
 
    long double add(long double input[], int length = 2)
@@ -129,24 +78,11 @@ private:
       {
          long double y = input[i] - sumOfError;
          long double t = sum + y;
-         long double c = (t - sum) - y;
+         sumOfError = (t - sum) - y;
 
          sum = t;
       }
 
       return sum;
-   }
-
-   bool checkDivisionError(long double before, long double after, int precisionLevel)
-   {
-      long beforeCeil = roundl(before * pow(10, precisionLevel + 1));
-      long afterCeil = roundl(after * pow(10, precisionLevel + 1));
-
-      if (beforeCeil == afterCeil)
-      {
-         return true;
-      }
-
-      return false;
    }
 };
--- TranscendentalEquationsSolver_Math/TranscendentalEquationsSolver_Math.cpp
+++ TranscendentalEquationsSolver_Math/TranscendentalEquationsSolver_Math.cpp
@@ -6,8 +6,7 @@ using namespace std;
 int main()
 {
    Solver* solver = new Solver(1, 3, -4, -1);
-   //cout.precision(5);
+   cout.precision(5);
    cout << solver->solve() << endl;
-   //cout << solver->solveDep() << endl;
 }
 
